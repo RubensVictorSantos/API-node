f6cfe3ed71c7c697aa94ef47ed1968cdd40f5bc7
Rubens Victor <rubensvictorsantos@outlook.com>
2020-03-04 16:28:55 -0300

    Fazendo backup do banco

--- backup pelo cmd.bat
+++ backup pelo cmd.bat
@@ -1,8 +1,8 @@
-ECHO Fazendo bkp do banco bd_site e salvando na pasta na area de trabalho 
+ECHO Fazendo bkp do banco bd_api e salvando na pasta na area de trabalho 
 c:
-cd "C:\Users\rubens\Desktop"
+cd "C:\Users\rubens\Desktop\pessoal\node\"
 md backup
 c:
 cd "C:\Program Files\MySQL\MySQL Server 5.7\bin" 
-mysqldump -u root -p db_site > c:\Users\rubens\Desktop\backup\db_site.sql 
+mysqldump -u root -p db_api > c:\Users\rubens\Desktop\pessoal\node\backup\db_api.sql 
 
