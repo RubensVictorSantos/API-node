f8e2332582603a6480f274953b350966ef43c89a
Rubens Victor <rubensvictorsantos@gmail.com>
2021-02-11 11:13:57 -0300

    backup banco 11/02/2021

--- banco/backup.bat
+++
@@ -1,7 +0,0 @@
-ECHO Fazendo bkp do banco bd_newsite 
-c:
-cd "C:\Users\rubens\Desktop\pessoal\nodejs\API-node\banco\"
-md script
-c:
-cd "C:\Program Files\MySQL\MySQL Server 5.7\bin" 
-mysqldump -u root -p db_newsite > c:\Users\rubens\Desktop\pessoal\nodejs\api-node\banco\db_newsite.sql 
